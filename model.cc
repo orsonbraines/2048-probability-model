68f24ebc689c65a5572555079093be0900e41111
Orson Baines <obraines@gmail.com>
2024-03-26 20:13:31 -0700

    finish model code (at least for now)

--- model.cc
+++ model.cc
@@ -13,17 +13,33 @@ GridState<8> s_grid8;
 std::mt19937 s_random((std::random_device())());
 
 
-int main(int argc, char** argv) {
-	std::cout << "2x2: " << GridState<2>::BITS_PER_TILE << " bits-per-tile " << GridState<2>::GRID_BITS << " bits-per-grid" << std::endl;
-	std::cout << "3x3: " << GridState<3>::BITS_PER_TILE << " bits-per-tile " << GridState<3>::GRID_BITS << " bits-per-grid" << std::endl;
-	std::cout << "4x4: " << GridState<4>::BITS_PER_TILE << " bits-per-tile " << GridState<4>::GRID_BITS << " bits-per-grid" << std::endl;
-	std::cout << "5x5: " << GridState<5>::BITS_PER_TILE << " bits-per-tile " << GridState<5>::GRID_BITS << " bits-per-grid" << std::endl;
-	std::cout << "6x6: " << GridState<6>::BITS_PER_TILE << " bits-per-tile " << GridState<6>::GRID_BITS << " bits-per-grid" << std::endl;
-	std::cout << "7x7: " << GridState<7>::BITS_PER_TILE << " bits-per-tile " << GridState<7>::GRID_BITS << " bits-per-grid" << std::endl;
-	std::cout << "8x8: " << GridState<8>::BITS_PER_TILE << " bits-per-tile " << GridState<8>::GRID_BITS << " bits-per-grid" << std::endl;
+int main() {
+	std::cout << "2x2: " << GridState<2>::BITS_PER_TILE << " bits-per-tile " << GridState<2>::GRID_BITS << " bits-per-grid " << sizeof(GridState<2>) << " bytes in memory." << std::endl;
+	std::cout << "3x3: " << GridState<3>::BITS_PER_TILE << " bits-per-tile " << GridState<3>::GRID_BITS << " bits-per-grid " << sizeof(GridState<3>) << " bytes in memory." << std::endl;
+	std::cout << "4x4: " << GridState<4>::BITS_PER_TILE << " bits-per-tile " << GridState<4>::GRID_BITS << " bits-per-grid " << sizeof(GridState<4>) << " bytes in memory." << std::endl;
+	std::cout << "5x5: " << GridState<5>::BITS_PER_TILE << " bits-per-tile " << GridState<5>::GRID_BITS << " bits-per-grid " << sizeof(GridState<5>) << " bytes in memory." << std::endl;
+	std::cout << "6x6: " << GridState<6>::BITS_PER_TILE << " bits-per-tile " << GridState<6>::GRID_BITS << " bits-per-grid " << sizeof(GridState<6>) << " bytes in memory." << std::endl;
+	std::cout << "7x7: " << GridState<7>::BITS_PER_TILE << " bits-per-tile " << GridState<7>::GRID_BITS << " bits-per-grid " << sizeof(GridState<7>) << " bytes in memory." << std::endl;
+	std::cout << "8x8: " << GridState<8>::BITS_PER_TILE << " bits-per-tile " << GridState<8>::GRID_BITS << " bits-per-grid " << sizeof(GridState<8>) << " bytes in memory." << std::endl;
 
-	s_grid4.writeTile(0,0,1);
-	s_grid4.writeTile(3,3,4);
-
-	std::cout << s_grid4 << std::endl;
+	double fourChance = 0.2;
+	// [right, up, left, down]*5
+	for(int i = 0; i < 5; ++i) {
+		s_grid3.genRand(fourChance);
+		std::cout << s_grid3 << std::endl;
+		std::cout << s_grid3.swipe(0,1) << " points gained! [RIGHT]" << std::endl;
+		std::cout << s_grid3 << std::endl;
+		s_grid3.genRand(fourChance);
+		std::cout << s_grid3 << std::endl;
+		std::cout << s_grid3.swipe(-1,0) << " points gained! [UP]" << std::endl;
+		std::cout << s_grid3 << std::endl;
+		s_grid3.genRand(fourChance);
+		std::cout << s_grid3 << std::endl;
+		std::cout << s_grid3.swipe(0,-1) << " points gained! [LEFT]" << std::endl;
+		std::cout << s_grid3 << std::endl;
+		s_grid3.genRand(fourChance);
+		std::cout << s_grid3 << std::endl;
+		std::cout << s_grid3.swipe(1,0) << " points gained! [DOWN]" << std::endl;
+		std::cout << s_grid3 << std::endl;
+	}
 }
--- model.h
+++ model.h
@@ -18,7 +18,23 @@ constexpr uint tileLog2(uint x) {
 	return 32u - __builtin_clz(x);
 }
 
-template<int N>
+template<uint N>
+class packed_bitset{
+public:
+	bool test(uint n) const {
+		assert(n < N);
+		return (m_data[n >> 3] & (1u << (n & 0x7))) != 0;
+	}
+	void set(uint n, bool v = true) {
+		assert(n < N);
+		if (v) m_data[n >> 3] |= (1u << (n & 0x7));
+		else m_data[n >> 3] &= ~(1u << (n & 0x7));
+	}
+private:
+	uint8_t m_data[(N+7)/8];
+};
+
+template<uint N, class BITSET_T=packed_bitset<tileLog2(N*N + 1) * N * N>>
 class GridState{
 public:
 	static constexpr uint BITS_PER_TILE = tileLog2(N*N + 1);
@@ -27,83 +43,226 @@ public:
 
 	uint readTile(uint row, uint col) const;
 	void writeTile(uint row, uint col, uint tile);
-	GridState<N> swipe(int dirRow, int dirCol) const;
-	GridState<N> genRand(double fourRatio) const;
+	uint swipe(int dirRow, int dirCol);
+	void genRand(double fourRatio);
 private:
 	void swap(uint row1, uint col1, uint row2, uint col2);
 	uint slideCol(uint col, bool dir);
 	uint slideRow(uint row, bool dir);
-	std::bitset<GRID_BITS> m_grid;
+	BITSET_T m_grid;
 };
 
-template<int N>
-void swap(uint row1, uint col1, uint row2, uint col2) {
-	//TODO
+template<uint N, class BITSET_T>
+void GridState<N, BITSET_T>::swap(uint row1, uint col1, uint row2, uint col2) {
+	uint tile1 = readTile(row1, col1);
+	uint tile2 = readTile(row2, col2);
+	writeTile(row1, col1, tile2);
+	writeTile(row2, col2, tile1);
 }
 
-template<int N>
-uint GridState<N>::slideCol(uint col, bool dir) {
+template<uint N, class BITSET_T>
+uint GridState<N, BITSET_T>::slideCol(uint col, bool dir) {
 	assert(col >= 0 && col < N);
 	uint sumOfNewTiles = 0;
 	bool lastSlideFused = false;
-	if(dir) { /*TODO*/ }
+	if(dir) {
+		for(int ri = N - 1; ri >= 0; --ri) {
+			for(int rf = ri + 1; rf < (int)N; ++rf) {
+				if(readTile(rf, col) == 0) {
+					// move tile up to empty space
+					swap(rf, col, rf - 1, col);
+					if(rf == 0) {
+						// set last slide fused to false if we are about to exit loop
+						lastSlideFused = false;
+					}
+				}
+				else if(readTile(rf, col) == readTile(rf - 1, col) && !lastSlideFused) {
+					// fuse with identical tile
+					lastSlideFused = true;
+					uint fusedTile = readTile(rf, col) + 1;
+					sumOfNewTiles += (1u << fusedTile);
+					writeTile(rf, col, fusedTile);
+					writeTile(rf - 1, col, 0);
+					break;
+				}
+				else {
+					// we collided with tile to which we cannot fuse
+					lastSlideFused = false;
+					break;
+				}
+			}
+		}
+	}
 	else {
-		for(uint ri = 1; ri < N; ++ri) {
-			for(uint rf = ri - 1; rf >= 0; --rf) {
-				// TODO
+		for(int ri = 1; ri < (int)N; ++ri) {
+			for(int rf = ri - 1; rf >= 0; --rf) {
+				if(readTile(rf, col) == 0) {
+					// move tile up to empty space
+					swap(rf, col, rf + 1, col);
+					if(rf == 0) {
+						// set last slide fused to false if we are about to exit loop
+						lastSlideFused = false;
+					}
+				}
+				else if(readTile(rf, col) == readTile(rf + 1, col) && !lastSlideFused) {
+					// fuse with identical tile
+					lastSlideFused = true;
+					uint fusedTile = readTile(rf, col) + 1;
+					sumOfNewTiles += (1u << fusedTile);
+					writeTile(rf, col, fusedTile);
+					writeTile(rf + 1, col, 0);
+					break;
+				}
+				else {
+					// we collided with tile to which we cannot fuse
+					lastSlideFused = false;
+					break;
+				}
 			}
 		}
 	}
 	return sumOfNewTiles;
 }
 
-template<int N>
-uint GridState<N>::slideRow(uint row, bool dir) {
+template<uint N, class BITSET_T>
+uint GridState<N, BITSET_T>::slideRow(uint row, bool dir) {
 	assert(row >= 0 && row < N);
 	uint sumOfNewTiles = 0;
-	// TODO
+	bool lastSlideFused = false;
+	if(dir) {
+		for(int ci = N - 1; ci >= 0; --ci) {
+			for(int cf = ci + 1; cf < (int)N; ++cf) {
+				if(readTile(row, cf) == 0) {
+					// move tile up to empty space
+					swap(row, cf, row, cf - 1);
+					if(cf == 0) {
+						// set last slide fused to false if we are about to exit loop
+						lastSlideFused = false;
+					}
+				}
+				else if(readTile(row, cf) == readTile(row, cf - 1) && !lastSlideFused) {
+					// fuse with identical tile
+					lastSlideFused = true;
+					uint fusedTile = readTile(row, cf) + 1;
+					sumOfNewTiles += (1u << fusedTile);
+					writeTile(row, cf, fusedTile);
+					writeTile(row, cf - 1, 0);
+					break;
+				}
+				else {
+					// we collided with tile to which we cannot fuse
+					lastSlideFused = false;
+					break;
+				}
+			}
+		}
+	}
+	else {
+		for(int ci = 1; ci < (int)N; ++ci) {
+			for(int cf = ci - 1; cf >= 0; --cf) {
+				if(readTile(row, cf) == 0) {
+					// move tile up to empty space
+					swap(row, cf, row, cf + 1);
+					if(cf == 0) {
+						// set last slide fused to false if we are about to exit loop
+						lastSlideFused = false;
+					}
+				}
+				else if(readTile(row, cf) == readTile(row, cf + 1) && !lastSlideFused) {
+					// fuse with identical tile
+					lastSlideFused = true;
+					uint fusedTile = readTile(row, cf) + 1;
+					sumOfNewTiles += (1u << fusedTile);
+					writeTile(row, cf, fusedTile);
+					writeTile(row, cf+1, 0);
+					break;
+				}
+				else {
+					// we collided with tile to which we cannot fuse
+					lastSlideFused = false;
+					break;
+				}
+			}
+		}
+	}
 	return sumOfNewTiles;
 }
 
-template<int N>
-uint GridState<N>::readTile(uint row, uint col) const {
+template<uint N, class BITSET_T>
+uint GridState<N, BITSET_T>::readTile(uint row, uint col) const {
+	assert(row < N && col < N);
 	uint tile = 0;
-	for(uint i = 0; i < GRID_BITS; ++i) {
-		tile |= (m_grid[(row * N + col) * GRID_BITS + i] << i);
+	for(uint i = 0; i < BITS_PER_TILE; ++i) {
+		tile |= (m_grid.test((row * N + col) * BITS_PER_TILE + i) << i);
 	}
 	return tile;
 }
 
-template<int N>
-void GridState<N>::writeTile(uint row, uint col, uint tile) {
-	for(uint i = 0; i < GRID_BITS; ++i) {
-		m_grid[(row * N + col) * GRID_BITS + i] = (tile & 1);
+template<uint N, class BITSET_T>
+void GridState<N, BITSET_T>::writeTile(uint row, uint col, uint tile) {
+	assert(row < N && col < N);
+	for(uint i = 0; i < BITS_PER_TILE; ++i) {
+		m_grid.set((row * N + col) * BITS_PER_TILE + i, tile & 1);
 		tile >>= 1;
 	}
 }
 
-template<int N>
-GridState<N> GridState<N>::swipe(int dirRow, int dirCol) const {
-	assert(((dirRow == -1 || dirRow == 1) && dirCol == 0) || ((dirCol == -1 || dirCol == 1) && dirCol == 0));
-	GridState nextState = *this;
-	return nextState;
+template<uint N, class BITSET_T>
+uint GridState<N, BITSET_T>::swipe(int dirRow, int dirCol) {
+	assert(((dirRow == -1 || dirRow == 1) && dirCol == 0) || ((dirCol == -1 || dirCol == 1) && dirRow == 0));
+	uint sumOfNewTiles = 0;
+	if(dirCol == 0) {
+		for(uint col = 0; col < N; ++col) {
+			sumOfNewTiles += slideCol(col, dirRow == 1);
+		}
+	}
+	else {
+		for(uint row = 0; row < N; ++row) {
+			sumOfNewTiles += slideRow(row, dirCol == 1);
+		}
+	}
+	return sumOfNewTiles;
 }
 
-template<int N>
-GridState<N> GridState<N>::genRand(double fourRatio) const {
-	GridState nextState = *this;
-	return nextState;
+
+// Don't call if a full grid
+template<uint N, class BITSET_T>
+void GridState<N, BITSET_T>::genRand(double fourRatio) {
+	std::uniform_real_distribution<double> d_dist;
+	std::uniform_int_distribution<uint> i_dist(0, N-1);
+	double roll = d_dist(s_random);
+	for(uint i = 0; i < 1000*N*N; ++i) {
+		uint row = i_dist(s_random);
+		uint col = i_dist(s_random);
+		if(readTile(row, col) == 0) {
+			writeTile(row, col, roll < fourRatio ? 2 : 1);
+			return;
+		}
+	}
+	// If we haven't found an empty tile in 1000N**2 attempts, the grid is probably full.
+	assert(0);
 }
 
-template<int N>
-std::ostream& operator<<(std::ostream& o, const GridState<N>& grid) {
+template<uint N, class BITSET_T>
+std::ostream& operator<<(std::ostream& o, const GridState<N, BITSET_T>& grid) {
 	for(uint r = 0; r < N; ++r) {
 		for(uint c = 0; c < N; c++) {
 			o.setf(std::ios::right);
   			o.width(8);
-			o << GridState<N>::tileToStr(grid.readTile(r,c));
+			o << GridState<N, BITSET_T>::tileToStr(grid.readTile(r,c));
 		}
 		o << std::endl;
 	}
 	return o;
 }
+
+template<uint N>
+class Game {
+public:
+	void reset() { m_state = GridState<N>(); }
+	uint getScore() const { return m_score; }
+	void swipe(int dirRow, int dirCol) { m_score += m_state.swipe(dirRow, dirCol); }
+private:
+	GridState<N> m_state;
+	uint m_score;
+};
